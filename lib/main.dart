0bc9e4f65b8f657538166c738edefcf4acb8f846
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-12-27 23:27:23 -0300

    bottom modal form created

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:personal_expences/components/transaction_user.dart';
+import 'package:personal_expences/components/transaction_form.dart';
+import 'package:personal_expences/components/transaction_list.dart';
+import 'package:personal_expences/models/transaction.dart';
 
 void main() => runApp(ExpensesApp());
 
@@ -14,15 +18,62 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'Aluguel', value: 1109.37, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Condomínio', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't3', title: '#01', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't4', title: '#02', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't5', title: '#03', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't6', title: '#04', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't7', title: '#05', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't8', title: '#06', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't9', title: '#07', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't10', title: '#08', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't11', title: '#09', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't12', title: '#10', value: 359.25, date: DateTime.now()),
+    Transaction(id: 't13', title: '#11', value: 359.25, date: DateTime.now())
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(addTransaction: _addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () => _openTransactionFormModal(context),
+              icon: Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -35,13 +86,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser()
+            TransactionList(transactions: _transactions),
           ],
         ),
       ),
-      floatingActionButton:
-          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _openTransactionFormModal(context)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
